c4da7e2dd094605a89e35f5fce7f61e8c1bf1149
shivashp <shivapandey04@gmail.com>
2017-10-31 16:41:54 +0545

    score and bg

--- tetris.js
+++ tetris.js
@@ -4,6 +4,23 @@ const context = canvas.getContext('2d');
 
 context.scale(20,20);
 
+function arenaSweep() {
+    let rowCount = 1;
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0 ) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
+
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y) {
@@ -128,6 +145,8 @@ function playerReset() {
     player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
     if (collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
 
@@ -152,6 +171,8 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();        
+        arenaSweep();   
+        updateScore();  
     }
     dropCounter = 0;
 }
@@ -191,6 +212,10 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
 const colors = [
     null, 
     '#FF0D72',
@@ -205,8 +230,9 @@ const colors = [
 const arena = createMatrix(12,20);
 
 const player = {
-    pos: {x: 5, y: 5},
-    matrix: createPiece('T')
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
 }
 
 document.addEventListener('keydown', event => {
@@ -223,4 +249,5 @@ document.addEventListener('keydown', event => {
     }
 });
 
+playerReset();
 update();
